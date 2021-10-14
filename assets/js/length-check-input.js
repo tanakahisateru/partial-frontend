6d39104aa93acb33993639272e674acb6579af4a
Hisateru Tanaka <tanakahisateru@gmail.com>
2021-10-14 22:08:39 +0900

    遅延のある atom 更新の単純な例

--- assets/js/length-check-input.js
+++ assets/js/length-check-input.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import LengthCheckInput from './components/LengthCheckInput';
 import {atom} from "jotai";
 
+function sleep(msec) {
+    return new Promise(resolve => setTimeout(resolve, msec));
+}
+
 $.fn.lengthCheckInput = function (options) {
     options = $.extend({
         max: 0,
@@ -11,10 +15,13 @@ $.fn.lengthCheckInput = function (options) {
 
     this.each(function () {
 
-        const textState = atom("");
-        const errorState = atom((get) => {
-            return get(textState).length > options.max;
+        const textState = atom("", (get, set, value) => {
+            set(textState, value);
+            sleep(1000).then(() => {
+                set(errorState, value.length > options.max);
+            });
         });
+        const errorState = atom(false);
 
         ReactDOM.render(createElement(LengthCheckInput, {
             id: this.id,
