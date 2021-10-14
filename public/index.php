376405bb7025b9b2e8a0868a6a4303e43708b849
Hisateru Tanaka <tanakahisateru@gmail.com>
2021-10-14 15:40:15 +0900

    従来の JS を Alpine.js で書くとどうなるか

--- public/index.php
+++ public/index.php
@@ -7,22 +7,17 @@ $manifest = json_decode(file_get_contents(__DIR__ . '/assets/dist/manifest.json'
     <meta charset="UTF-8">
     <title>Partial Frontend</title>
     <link rel="stylesheet" href="<?= $manifest['bundle.css'] ?>">
+    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
 </head>
 <body class="index-page">
 
-<div class="length-check-input">
+<h1>Alpine.js どうかな</h1>
+<div class="length-check-input" x-data="{ overflow: false }">
     <label for="text">Text</label>
-    <input id="text" type="text" value="">
+    <input id="text" type="text" value="" x-on:input="overflow = $event.target.value.length > 4">
+    <div class="error" style="display: none;" x-show="overflow">長すぎます。4文字以下で入力してください。</div>
 </div>
 
 <script src="<?= $manifest['bundle.js'] ?>"></script>
-<script>
-$(function () {
-    $('.length-check-input').lengthCheckInput({
-        max: 4,
-        message:"長すぎます。4文字以下で入力してください。"
-    });
-});
-</script>
 </body>
 </html>
